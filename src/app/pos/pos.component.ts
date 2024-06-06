3efe365da73113c70cf7b7c8e9ab31b9c2040672
Suman Tamang <sumantamang1020@gmail.com>
2024-06-06 18:36:20 +0930

    added add/remove drink option feature

--- src/app/pos/pos.component.ts
+++ src/app/pos/pos.component.ts
@@ -18,6 +18,7 @@ export class PosComponent {
 
   saleItems: coffee[] = [];
   selectedSaleItemIndex = -1;
+  selectedSaleItemOptionIndex = -1;
   buttons: string[] = ['Extra Hot', 'Luke Warm', '1/2 Strength', '+1 shot', ];
 
 
@@ -43,15 +44,24 @@ export class PosComponent {
   }
 
   onRemoveSaleItem( ){
-    if (this.selectedSaleItemIndex < 0){
+    if (this.selectedSaleItemIndex < 0 && this.selectedSaleItemOptionIndex < 0){
       return;
     } 
+    if (this.selectedSaleItemOptionIndex > -1 ) {
+      this.saleItems[this.selectedSaleItemIndex].Comment.splice(this.selectedSaleItemOptionIndex, 1);
+      this.selectedSaleItemOptionIndex = -1;
+      return;
+    }
     this.saleItems.splice(this.selectedSaleItemIndex, 1);
+    this.selectedSaleItemIndex = -1;
   }
 
   onClickedComment(comment: string){
-    console.log(comment);
     this.saleItems[this.selectedSaleItemIndex].Comment.push(comment);
+  }
+
+  onSelectedSaleItemOption(index: number){
+    this.selectedSaleItemOptionIndex = index;
   }
 
 }
